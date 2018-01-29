8d672f3ede24a4e7a0ebb360240ccbcffe6a8419
ktan17 <Kevin.Tan@gpiglobal.com>
2018-01-28 19:17:24 -0800

    added starter code

---
+++ PrettyGoodPokedexStarter/PrettyGoodPokedex/PokemonTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  PrettyGoodPokedex
+//
+//  Created by Kevin Tan on 1/28/18.
+//  Copyright © 2018 ACM Hack. All rights reserved.
+//
+
+import UIKit
+
+class PokemonTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UITableViewDelegate, UITableViewController methods
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // TODO
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO
+    }
+
+}
+
